1f3689bf5f4861917afde3473098993eec9b2618
0214kbs <0214kbs@naver.com>
2023-08-21 16:56:32 +0900

    Refactor: Profile부분 분리

---
+++ src/pageComponents/home/profile/Profile.styled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+const StyledImg = styled.img`
+  border-radius: 1rem;
+  width: 290px;
+  height:390.938px;
+`
+const StyledRight = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  `
+
+const StyledText = styled.div`
+  font-family:${(props) => props.theme.fonts.HangeulFontRegular};
+`
+export { StyledImg, StyledRight, StyledText }  
