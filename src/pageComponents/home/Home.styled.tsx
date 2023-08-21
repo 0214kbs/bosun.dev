93366d914b3e2c9825df34498dc04b398ddfacd6
0214kbs <0214kbs@naver.com>
2023-08-21 16:17:49 +0900

    Feat: Main page의 Profile부분 완성

--- src/pageComponents/home/Home.styled.tsx
+++ src/pageComponents/home/Home.styled.tsx
@@ -1,14 +1,28 @@
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
-  max-width: 730px;
-  display: flex;
-  flex-wrap: nowrap;
-  align-items: center;
-  justify-content: space-between;
+  max-width: 740px;
   margin: auto;
   padding: 0 8px;
 
 `;
 
-export { StyledContainer }  
+const StyledImg = styled.img`
+  border-radius: 1rem;
+  width: 300px;
+  height:auto;
+`
+const StyledRight = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  `
+
+const StyledText = styled.div`
+  font-family:${(props) => props.theme.fonts.HangeulFontRegular};
+  
+
+`
+export { StyledContainer, StyledImg, StyledRight, StyledText }  
--- src/pageComponents/home/index.tsx
+++ src/pageComponents/home/index.tsx
@@ -1,11 +1,39 @@
 "use client"
-import { StyledContainer } from "./Home.styled"
-
+import theme from "@/styles/DefaultTheme"
+import { StyledContainer, StyledImg, StyledRight, StyledText } from "./Home.styled"
 const HomePage = () => {
     return (
         <StyledContainer>
+            <h1 style={{ fontFamily: theme.fonts.HangeulFontMedium }}> About Me </h1>
+            {/* <p> Let me introduce myself </p> */}
+            <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
+                <StyledImg src="/profile.jpg" alt="profile" />
+                <StyledRight>
 
-            <div>Home</div>
+                    <div>
+
+                        <StyledText style={{ fontWeight: "bold" }}>
+                            👋🏻 끊임없이 성장하고, 프론트엔드 전문성을 깊게 추구합니다.
+                        </StyledText>
+                        <StyledText style={{ color: "gray", marginLeft: "15px", padding: "10px" }}>
+                            모든 도전을 새로운 학습의 기회로 받아들이며,
+                            프론트엔드 분야의 최신 트렌드와 기술에 항상 집중하고 있습니다.
+                            <br />
+                            <br />
+                            이를 바탕으로 더욱 전문적인 개발자로 성장하기 위한 지속적인 노력을 기울이고 있습니다.
+                        </StyledText>
+                    </div>
+                    <div>
+                        <StyledText style={{ fontWeight: "bold" }}>
+                            ✈️ 여행을 좋아합니다.
+                        </StyledText>
+                        <StyledText style={{ marginLeft: "15px", padding: "10px", color: "gray" }}>
+                            현재까지 다녀온 나라
+                            <span style={{ color: theme.colors.gray, fontFamily: theme.fonts.HangeulFontRegular }}> Guam, Hawaii, Italy, Czech Republic, Austria, Canada, United States</span>
+                        </StyledText>
+                    </div>
+                </StyledRight>
+            </div>
         </StyledContainer>
     )
 }
