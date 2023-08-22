214e095634ba9a0cfd790dfc7026e9a7a83bf47f
0214kbs <0214kbs@naver.com>
2023-08-22 10:29:38 +0900

    Chore: contentlayer 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+import { withContentlayer } from 'next-contentlayer';
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: false,
   compiler: {
     styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
   },
 };
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);
