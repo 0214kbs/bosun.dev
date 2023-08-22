71fec4ef822cca77b41835343499c6f1668b3362
0214kbs <0214kbs@naver.com>
2023-08-23 00:14:57 +0900

    Fix: blog post metadata 임시로 default 지정

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,23 +5,11 @@ import { usePathname } from "next/navigation";
 import Blogpost from "@/pageComponents/blogpost";
 import { Metadata } from "next";
 
-export async function generateMetadata(): Promise<Metadata> {
-	const slug = usePathname().split("/").pop();
-	const post = allPosts.find((p) => p._raw.flattenedPath === slug);
-	console.log(post);
-	if (!post) {
-		return {
-			title: "no blog post",
-			description: "현재 경로가 잘못 되었거나, 삭제된 게시물입니다.",
-			authors: [{ name: "BosunKwak", url: "https://bosun-dev.vercel.app/" }],
-		};
-	}
-	return {
-		title: post.title,
-		description: post.description,
-		authors: [{ name: "BosunKwak", url: "https://bosun-dev.vercel.app/" }],
-	};
-}
+export const metadata: Metadata = {
+	title: "Bosun's Blog",
+	description: "nextjs, typescript 등으로 만든 blog입니다. ",
+	authors: [{ name: "BosunKwak", url: "https://bosun-dev.vercel.app/" }],
+};
 
 function Post() {
 	const slug = usePathname().split("/").pop();
