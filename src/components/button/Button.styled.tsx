ae5adf62ee4ff2c9deade7e51099a5c60ed30b23
0214kbs <0214kbs@naver.com>
2023-08-23 14:00:14 +0900

    Feat: 공통 Button component 추가

---
+++ src/components/button/Button.styled.tsx
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+import { ButtonStyles } from "./button.type";
+
+const StyledButton = styled.button.attrs<ButtonStyles>((props) => ({}))`
+  ${(props) => {
+    const main = props.theme.colors.main;
+    const bgcolor = props.bgcolor || main;
+    const color = props.color || "white";
+    const lightgray = props.theme.colors.lightgray;
+    return css`
+      cursor: pointer;
+      font-size: 16px;
+      border: 1px solid ${lightgray};
+      line-height: 1;
+      height: 45px;
+      padding: 10px;
+      width:100%;
+      background-color: ${bgcolor};
+      color: ${color};
+      border-radius: 10px;
+        &:hover{
+          opacity:0.5;
+        }
+      `;
+  }};
+`;
+
+export { StyledButton };
---
+++ src/components/button/Button.type.ts
@@ -0,0 +1,9 @@
+export interface ButtonStyles {
+  color?: string;
+  label?: string;
+  bgcolor?: string;
+
+  onClick?: (e: any) => any;
+  type?: "button" | "submit" | "reset";
+  style?: any;
+}
