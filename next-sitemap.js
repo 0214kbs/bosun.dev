261377221a879cd4088ab05b2f3d663075abeae4
0214kbs <0214kbs@naver.com>
2023-08-28 12:43:16 +0900

    Feat: sitemap url 수정

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 // export const siteUrl = "https://bosunsblog.com";
 // export const generateRobotsTxt = true;
 module.exports = {
-	siteUrl: "https://bosunsblog.com",
+	siteUrl: "https://bosun-dev.vercel.app",
 	generateRobotsTxt: true,
 };
