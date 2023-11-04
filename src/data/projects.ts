144ea99479f3c90207b08a889a3cae05aed20003
0214kbs <0214kbs@naver.com>
2023-11-04 17:16:29 +0900

    Feat: 블로그 부분 잠시 지우고 포트폴리오 업데이트

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,52 +1,75 @@
 export const projects = [
-    {
-        title: "Bosun Kwak's blog",
-        date: "2023.08.21 ~ 진행중 (1명)",
-        imgsrc: "/projects/blogmain.png",
-        content: "저만의 블로그 및 포트폴리오 입니다.",
-        stacks: ["/stack/nextjs.svg", "/stack/react.svg", "/stack/typescript.svg", "/stack/styledcomponents-color.svg", "/stack/storybook-color.svg", "/stack/node.svg"],
-        gitlink: "https://github.com/0214kbs/bosun.dev",
-        link: "https://bosun-dev.vercel.app/"
-    },
-    {
-        title: "SSS(Special Survey Service)",
-        date: "2023.07.28 ~ 2023.08.18 (6명)",
-        imgsrc: "/projects/sss.png",
-        content: "공정한 설문 조사 유도 서비스입니다.",
-        stacks: ["/stack/nextjs.svg", "/stack/react.svg", "/stack/typescript.svg", "/stack/styledcomponents-color.svg", "/stack/node.svg"],
-        gitlink: "https://github.com/0214kbs/SSS",
-        link: "https://sss-azure.vercel.app/"
-    },
-    {
-        title: "Design System",
-        date: "2023.07.11 ~ 진행중 (1명)",
-        imgsrc: "/projects/designsystem.PNG",
-        content: "storybook을 이용해 시각적, 기능적 컴포넌트 및 가이드라인을 정의한 Design System 입니다.",
-        stacks: ["/stack/nextjs.svg", "/stack/react.svg", "/stack/typescript.svg", "/stack/styledcomponents-color.svg", "/stack/storybook-color.svg", "/stack/node.svg"],
-        gitlink: "https://github.com/0214kbs/design-system"
-    },
-    {
-        title: "Trip",
-        date: "2023.05.16 ~ 2023.05.26 (2명)",
-        imgsrc: "/projects/enjoytrip.png",
-        content: "여행 일정을 계획하는 웹 프로젝트입니다.",
-        stacks: ["/stack/vue.svg", "/stack/spring-color.svg", "/stack/mysql-color.svg"],
-        gitlink: "https://github.com/0214kbs/trip"
-    },
-    {
-        title: "IoT Malware Prediction",
-        date: "2022.12.08 ~ 2022.12.23 (6명)",
-        imgsrc: "/projects/aiprj.png",
-        content: "변종 악성코드에 대한 대비를 위한 최적의 악성코드 분류 모델 생성 프로젝트입니다.",
-        stacks: ["/stack/python-color.svg", "/stack/pandas-color.svg", "/stack/scikitlearn-color.svg"],
-        postlink: "https://docs.google.com/presentation/d/1fk4Ibf3iJeZMl80REqDOVnXGhMbsRvEJ/edit?usp=sharing&ouid=101191707180351934779&rtpof=true&sd=true"
-    },
-    {
-        title: "개집사",
-        date: "2021.07.26 ~ 2021.08.07 (5명)",
-        imgsrc: "/projects/dogapp.png",
-        content: "반려견을 두고 외출해야하는 사람들을 위한 동네 강아지 산책 서비스입니다.",
-        stacks: ["/stack/swift-color.svg", "/stack/firebase-color.svg", "/stack/xcode-color.svg"],
-        gitlink: "https://github.com/0214kbs/Gaejibsa-chatting"
-    }
+	{
+		title: "와이라노사우르스",
+		date: "2023.08.21 ~ 2023.10.06 (6명)",
+		imgsrc: "/projects/와이라노.png",
+		content: "고성 공룡 엑스포를 중심으로 어린아이를 동반한 가족 맞춤 관광 정보 제공 서비스",
+		stacks: ["/stack/react.svg", "/stack/typescript.svg", "/stack/styledcomponents-color.svg", "/stack/storybook-color.svg", "/stack/node.svg"],
+		gitlink: "https://github.com/0214kbs/Yrannosaurus",
+	},
+
+	{
+		title: "Bosun Kwak's blog",
+		date: "2023.08.21 ~ 진행중 (1명)",
+		imgsrc: "/projects/blogmain.png",
+		content: "나만의 블로그 및 포트폴리오",
+		stacks: [
+			"/stack/nextjs.svg",
+			"/stack/react.svg",
+			"/stack/typescript.svg",
+			"/stack/styledcomponents-color.svg",
+			"/stack/storybook-color.svg",
+			"/stack/node.svg",
+		],
+		gitlink: "https://github.com/0214kbs/bosun.dev",
+		link: "https://bosun-dev.vercel.app/",
+	},
+	{
+		title: "SSS(Special Survey Service)",
+		date: "2023.07.28 ~ 2023.08.18 (6명)",
+		imgsrc: "/projects/sss.png",
+		content: "공정한 설문 조사 유도 서비스",
+		stacks: ["/stack/nextjs.svg", "/stack/react.svg", "/stack/typescript.svg", "/stack/styledcomponents-color.svg", "/stack/node.svg"],
+		gitlink: "https://github.com/0214kbs/SSS",
+	},
+	{
+		title: "Design System",
+		date: "2023.07.11 ~ 진행중 (1명)",
+		imgsrc: "/projects/designsystem.PNG",
+		content: "storybook을 이용해 시각적, 기능적 컴포넌트 및 가이드라인을 정의한 Design System",
+		stacks: [
+			"/stack/nextjs.svg",
+			"/stack/react.svg",
+			"/stack/typescript.svg",
+			"/stack/styledcomponents-color.svg",
+			"/stack/storybook-color.svg",
+			"/stack/node.svg",
+		],
+		gitlink: "https://github.com/0214kbs/design-system",
+	},
+	{
+		title: "Trip",
+		date: "2023.05.16 ~ 2023.05.26 (2명)",
+		imgsrc: "/projects/enjoytrip.png",
+		content: ".여행 일정을 계획하는 웹 프로젝트",
+		stacks: ["/stack/vue.svg", "/stack/spring-color.svg", "/stack/mysql-color.svg"],
+		gitlink: "https://github.com/0214kbs/trip",
+	},
+	{
+		title: "IoT Malware Prediction",
+		date: "2022.12.08 ~ 2022.12.23 (6명)",
+		imgsrc: "/projects/aiprj.png",
+		content: "변종 악성코드에 대한 대비를 위한 최적의 악성코드 분류 모델 생성 프로젝트",
+		stacks: ["/stack/python-color.svg", "/stack/pandas-color.svg", "/stack/scikitlearn-color.svg"],
+		postlink:
+			"https://docs.google.com/presentation/d/1fk4Ibf3iJeZMl80REqDOVnXGhMbsRvEJ/edit?usp=sharing&ouid=101191707180351934779&rtpof=true&sd=true",
+	},
+	{
+		title: "개집사",
+		date: "2021.07.26 ~ 2021.08.07 (5명)",
+		imgsrc: "/projects/dogapp.png",
+		content: "반려견을 두고 외출해야하는 사람들을 위한 동네 강아지 산책 서비스",
+		stacks: ["/stack/swift-color.svg", "/stack/firebase-color.svg", "/stack/xcode-color.svg"],
+		gitlink: "https://github.com/0214kbs/Gaejibsa-chatting",
+	},
 ];
