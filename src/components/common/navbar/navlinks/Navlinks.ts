278c5ea11896793adac075f50e95789a035e7b04
0214kbs <0214kbs@naver.com>
2023-12-09 03:15:59 +0900

    Chore: 블로그 부분 잠시 숨기기 -  Notion API 연동 예정

--- src/components/common/navbar/navlinks/Navlinks.ts
+++ src/components/common/navbar/navlinks/Navlinks.ts
@@ -1,6 +1,6 @@
 const navlinks: { title: string; location: string }[] = [
 	{ title: "About", location: "/" },
-	{ title: "Blog", location: "/blog" },
+	// { title: "Blog", location: "/blog" },
 	{ title: "Projects", location: "/projects" },
 ];
 
