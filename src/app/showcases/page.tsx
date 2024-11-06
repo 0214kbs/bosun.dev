cbff0359b331d518548ffc50d1fbee321871c063
0214kbs <0214kbs@naver.com>
2024-11-06 15:22:36 +0900

    Feat: Showcases 페이지 추가

---
+++ src/app/showcases/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Project Page",
+	description: "Bosun Kwak의 컴포넌트 모음 입니다. ",
+	authors: [{ name: "BosunKwak", url: "https://bosun-dev.vercel.app/" }],
+};
+
+export default function Showcases() {
+	return <div></div>;
+}
