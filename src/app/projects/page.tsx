df83cfe244e4e79a277c1dec3510ae20f3d8abec
0214kbs <0214kbs@naver.com>
2024-11-08 14:13:08 +0900

    Refactor: pageComponents/projects 내용을 page.tsx에 포함

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import PageTitle from "@/components/common/pagetitle";
-import ProjectPage from "@/pageComponents/projects";
+import Projectcard from "@/components/common/projectcard";
 import { Metadata } from "next";
+import { projects } from "@/data/projects";
 
 export const metadata: Metadata = {
 	title: "Project Page",
@@ -12,7 +13,21 @@ export default function Projects() {
 	return (
 		<div style={{ maxWidth: "1030px", margin: "auto", padding: "0 8px", paddingTop: "6.5rem" }}>
 			<PageTitle title="Projects" text="진행한 프로젝트 모음입니다."></PageTitle>
-			<ProjectPage />
+			<div style={{ marginTop: "40px", display: "flex", justifyContent: "flex-start", flexWrap: "wrap", gap: "15px" }}>
+				{projects.map((project) => (
+					<Projectcard
+						key={project.title}
+						title={project.title}
+						date={project.date}
+						imgsrc={project.imgsrc}
+						content={project.content}
+						stacks={project.stacks}
+						gitlink={project.gitlink}
+						link={project.link}
+						postlink={project.postlink}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
--- src/components/common/projectcard/index.tsx
+++ src/components/common/projectcard/index.tsx
@@ -1,44 +1,51 @@
-import Link from "next/link"
-import { StyledProjectcard, StyledDate, StyledPrjTitle, StyledContentBox, StyledLinkItem, StyledLinkName } from "./Projectcard.styled"
-import { ProjectcardProps } from "./Projectcard.type"
+"use client";
+import Link from "next/link";
+import { StyledProjectcard, StyledDate, StyledPrjTitle, StyledContentBox, StyledLinkItem, StyledLinkName } from "./Projectcard.styled";
+import { ProjectcardProps } from "./Projectcard.type";
 
 const Projectcard = (props: ProjectcardProps) => {
-    return (
-        <StyledProjectcard>
-            <img src={props.imgsrc} alt="projectsimg" style={{ width: "290px", height: "180px", borderRadius: "1rem" }} />
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                <StyledDate> {props.date} </StyledDate>
-                <StyledPrjTitle>{props.title}</StyledPrjTitle>
-            </div>
-            <StyledContentBox>{props.content}</StyledContentBox>
-            <div style={{ display: "flex", gap: "10px" }}>
-                {props.stacks.map((item, index) => (
-                    <img key={index} src={item} alt={`stack-${index}`} style={{ width: "25px" }} />
-                    // <StyledStack key={index}>{item}</StyledStack>
-                ))}
-            </div>
-            <div style={{ display: "flex", gap: "12px" }}>
-                {props.link && <Link href={props.link} style={{ textDecoration: 'none', color: "black", display: "flex" }}>
-                    <StyledLinkItem>
-                        <img src="/link.png" style={{ width: "20px", height: "20px", marginRight: "5px" }}></img>
-                        <StyledLinkName>바로가기</StyledLinkName>
-                    </StyledLinkItem>
-                </Link>}
-                {props.gitlink && <Link href={props.gitlink} style={{ textDecoration: 'none', color: "black", display: "flex" }}>
-                    <StyledLinkItem>
-                        <img src="/github.png" style={{ width: "20px", height: "20px", marginRight: "5px" }}></img>
-                        <StyledLinkName>Github</StyledLinkName>
-                    </StyledLinkItem>
-                </Link>}
-                {props.postlink && <Link href={props.postlink} style={{ textDecoration: 'none', color: "black", display: "flex" }}>
-                    <StyledLinkItem>
-                        <img src="/post.png" style={{ width: "20px", height: "20px", marginRight: "5px" }}></img>
-                        <StyledLinkName>관련 포스트</StyledLinkName>
-                    </StyledLinkItem>
-                </Link>}
-            </div>
-        </StyledProjectcard >
-    )
-}
+	return (
+		<StyledProjectcard>
+			<img src={props.imgsrc} alt="projectsimg" style={{ width: "290px", height: "180px", borderRadius: "1rem" }} />
+			<div style={{ display: "flex", flexDirection: "column" }}>
+				<StyledDate> {props.date} </StyledDate>
+				<StyledPrjTitle>{props.title}</StyledPrjTitle>
+			</div>
+			<StyledContentBox>{props.content}</StyledContentBox>
+			<div style={{ display: "flex", gap: "10px" }}>
+				{props.stacks.map((item, index) => (
+					<img key={index} src={item} alt={`stack-${index}`} style={{ width: "25px" }} />
+					// <StyledStack key={index}>{item}</StyledStack>
+				))}
+			</div>
+			<div style={{ display: "flex", gap: "12px" }}>
+				{props.link && (
+					<Link href={props.link} style={{ textDecoration: "none", color: "black", display: "flex" }}>
+						<StyledLinkItem>
+							<img src="/link.png" style={{ width: "20px", height: "20px", marginRight: "5px" }}></img>
+							<StyledLinkName>바로가기</StyledLinkName>
+						</StyledLinkItem>
+					</Link>
+				)}
+				{props.gitlink && (
+					<Link href={props.gitlink} style={{ textDecoration: "none", color: "black", display: "flex" }}>
+						<StyledLinkItem>
+							<img src="/github.png" style={{ width: "20px", height: "20px", marginRight: "5px" }}></img>
+							<StyledLinkName>Github</StyledLinkName>
+						</StyledLinkItem>
+					</Link>
+				)}
+				{props.postlink && (
+					<Link href={props.postlink} style={{ textDecoration: "none", color: "black", display: "flex" }}>
+						<StyledLinkItem>
+							<img src="/post.png" style={{ width: "20px", height: "20px", marginRight: "5px" }}></img>
+							<StyledLinkName>관련 포스트</StyledLinkName>
+						</StyledLinkItem>
+					</Link>
+				)}
+			</div>
+		</StyledProjectcard>
+	);
+};
 
-export default Projectcard
+export default Projectcard;
