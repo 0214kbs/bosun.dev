4d5a1fda579b6eb0040b68420312290d8066bd11
0214kbs <0214kbs@naver.com>
2024-11-11 23:59:46 +0900

    Feat: Scratch 컴포넌트 생성 및 적용

--- src/app/showcases/[slug]/page.tsx
+++ src/app/showcases/[slug]/page.tsx
@@ -1,7 +1,26 @@
 "use client";
+import PageTitle from "@/components/common/pagetitle";
+import Scratch from "@/components/showcase/scratch";
+import { showcase_items } from "@/data/showcases";
 import { usePathname } from "next/navigation";
 
 export default function ShowCasePage() {
 	const slug = usePathname().split("/").pop();
-	return <div> test</div>;
+	const currentItem = showcase_items.find((item) => item.slug === slug);
+
+	let componentToShow;
+	switch (slug) {
+		case "scratch":
+			componentToShow = <Scratch beforeImage="/scratch/before_scratch.png" afterImage="/scratch/after_scratch.png" />;
+			break;
+		default:
+			componentToShow = <div> 다시 돌아가주세요..</div>;
+	}
+
+	return (
+		<div style={{ height: `calc(100vh - 215px)`, paddingTop: "6.5rem" }}>
+			<PageTitle title={currentItem?.title || "404"} text={currentItem?.des || "페이지를 찾을 수 없습니다."}></PageTitle>
+			<div style={{ flex: 1, height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>{componentToShow}</div>
+		</div>
+	);
 }
